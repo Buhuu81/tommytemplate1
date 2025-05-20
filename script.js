c91ebd473db8363d484202dab4d0dcbe5a4861eb
Buhuu81 <tommyjohansen1981@gmail.com>
2025-05-20 10:32:27 +0200

    Dag 9

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("hello")
+// console referer til inspiser vinduet .log gir en beskjed til console
+console.log("Hello World!");
+
+// Datatyper:
+// string = streng/tekst
+// number = tall
+// boolean = true/false sant/usant verdi
+
+// Variabler:
+// let = kan bytte verdi
+// const = ikke bytte verdi
+
+// Erklærer en variabel:
+// let ? = ?
+
+const userName = "Tommy"; //String
+const UserAge = "43"
+let userPresent = true;
+console. log("mitt navn er" + userName + ". Jeg er" + UserAge + "år gammek. Og det er" userPresent + "at jeg er tilstede på Fontenehuset i dag!");
+
+
+// Regular funkction
+function helloFunction() {
+    console. log;
+}
+
+//
+//
+
+function clickButton () {
+    const h1Element = document.getElementById ("changeText"); // mulig å bruke denne metoden også for å forandre  document.querySelector("#changeText")
+    console. log(h1Element);
+    const newText = "Welcome to my home page";
+
+}
+clickButton();
